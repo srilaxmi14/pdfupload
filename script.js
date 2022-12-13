2830fb11eb8d32b21a5c80ce112f09b49ab07535
Shetty <srilaxmi.shetty@wipfli.com>
2022-12-13 09:12:49 +0530

    upload

--- script.js
+++ script.js
@@ -7,13 +7,43 @@
 // })
 
 
-var form=document.querySelector("#selectFile");
-var fileInput=document.querySelector("#fileInput");
+// var form=document.querySelector("#selectFile");
+// var fileInput=document.querySelector("#fileInput");
 
-// form.addEventListenerp("submit",()=>{
+// // form.addEventListenerp("submit",()=>{
 
+// // })
+
+// form.addEventListener("submit",e=>{
+//     var file=e.target.files[0];
+//     if(file.type!='.pdf')
+//     {
+//         console.log("It is not a pdf file.");
+//         return;
+//     }
+// })
+// fileInput.onchange=e=>{
+//     console.log(e); 
+// }
+
+
+console.log($);
+
+// $('#button').click(function(){
+//     console.log("button is clicked");
+//     if(file.type!=".pdf")
+//     {
+//         console.log("not pdf");
+//         alert("it is not a pdf ");
+//     }
 // })
 
-fileInput.onchange=e=>{
-    console.log(e);
-}
+
+function mypdf() {
+  $('form').submit(function (e) {
+    e.preventDefault();
+    // $('input[type="file"]');
+    window.open(mypdf,"_blank");
+    this.submit();
+  });
+};
