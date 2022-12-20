b3e5e4b131687de7449c6779050e8adffd2ff1d3
Shetty <srilaxmi.shetty@wipfli.com>
2022-12-20 19:46:38 +0530

    upload

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
 
 $('#button').click(function (e) {
-  
+
   e.preventDefault();
   $(this).val("view another");
-  var target=$("#fileInput").get(0);
+  var target = $("#fileInput").get(0);
   console.log(target);
-  if(!target.files.length)
-  {
+
+  if (!target.files.length) {
     return;
   }
-  file=target.files[0];
-  console.log("main",file);
- 
+  file = target.files[0];
+  console.log("main", file);
+  console.log("list", file.name);
+  // var fileName=$(this).prevAll('fileInput').val().
+  var fileName = $(this).prevAll("input[type=file]").val();
+  console.log("list of file:", fileName);
 
-  if(file.type!=="application/pdf")
-  {
+
+  if (file.type !== "application/pdf") {
     alert("Allow pdf files only");
     return;
   }
 
-  var reader=new FileReader();
+  var reader = new FileReader();
   var results;
   // reading as a text
   reader.readAsDataURL(file);
   console.log(reader);
   console.log(file);
-  reader.onload=function()
-  {
-      results=reader.result;
-      console.log("results",results);
-      $("#pdfRender").html(`<iframe id="myFrame" src="${results}" style="height:500px;width:600px;" title="Iframe Example"></iframe>`);
+  reader.onload = function () {
+    results = reader.result;
+    console.log("results", results);
+    $("#pdfRender").html(`<iframe id="myFrame" src="${results}"  title="Iframe Example"></iframe>`);
   }
 
+  // $('#selectFile').submit(function(e){
+  //   var val = $(this).find('#fileInput').val();
+  //   $('ul.list').append('<li>' + val + '</li>');
+  //   console.log(val);
+  //   e.preventDefault();
+  // });
+
 });
+
+
+
+
